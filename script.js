30be62e4160d93b2008bddce23f8c5b28acd2daf
Hoang <hoang.viet.nguyen@outlook.com>
2021-01-12 16:26:27 -0500

    added autocomplete for input boxes

--- script.js
+++ script.js
@@ -13,7 +13,36 @@ function initMap() {
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: start,
-    zoom: 15,
+    zoom: 13,
+  });
+
+  let startInput = document.getElementById('start__input');
+  let endInput = document.getElementById('end__input');
+  var autocompleteOptions = {
+    componentRestrictions: { country: 'ca' },
+  };
+  let startAutocomplete = new google.maps.places.Autocomplete(
+    startInput,
+    autocompleteOptions
+  );
+  let endAutocomplete = new google.maps.places.Autocomplete(
+    endInput,
+    autocompleteOptions
+  );
+
+  let destinationSubmit = document.getElementById('destination__submit');
+
+  var options = {
+    types: ['(cities)'],
+    componentRestrictions: { country: 'fr' },
+  };
+
+  destinationSubmit.addEventListener('click', () => {
+    startPlace = startAutocomplete.getPlace();
+    endPlace = endAutocomplete.getPlace();
+    console.log(startPlace, endPlace);
+    createMarker(startPlace);
+    createMarker(endPlace);
   });
 }
 
@@ -41,23 +70,24 @@ function codeAddressCallback(results, status) {
     // request
     var request = {
       location: results[0].geometry.location,
-      radius: '500',
+      radius: '5000',
       query: '',
       type: 'gas_station',
     };
 
-    service = new google.maps.places.PlacesService(map);
-    service.textSearch(request, textSearchCallback);
+    //service = new google.maps.places.PlacesService(map);
+    //service.nearbySearch(request, textSearchCallback);
   } else {
     alert('Geocode was not successful for the following reason: ' + status);
   }
 }
 
 function textSearchCallback(results, status) {
+  console.log(results);
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       var place = results[i];
-      createMarker(results[i]);
+      createMarker(place);
     }
   }
 }
