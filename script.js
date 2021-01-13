b98247d829858305b684e19ed0c12ca8ae6fdf31
Hoang <hoang.viet.nguyen@outlook.com>
2021-01-12 23:05:40 -0500

    added distance matrix functionality

--- script.js
+++ script.js
@@ -3,14 +3,19 @@
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
 let map;
-let service;
+let placeService;
+let distanceMatrixService;
 let geocoder;
 let directionsService;
 let directionsRenderer;
+let startPlace;
+let endPlace;
 
 function initMap() {
   directionsService = new google.maps.DirectionsService();
   directionsRenderer = new google.maps.DirectionsRenderer();
+  distanceMatrixService = new google.maps.DistanceMatrixService();
+
   geocoder = new google.maps.Geocoder();
   let start = new google.maps.LatLng(56.1304, -106.3468);
 
@@ -19,6 +24,7 @@ function initMap() {
     zoom: 4,
   });
   directionsRenderer.setMap(map);
+  placeService = new google.maps.places.PlacesService(map);
 
   let startInput = document.getElementById('start__input');
   let endInput = document.getElementById('end__input');
@@ -36,20 +42,25 @@ function initMap() {
 
   let destinationSubmit = document.getElementById('destination__submit');
 
-  var options = {
-    types: ['(cities)'],
-    componentRestrictions: { country: 'fr' },
-  };
-
   destinationSubmit.addEventListener('click', () => {
     startPlace = startAutocomplete.getPlace();
     endPlace = endAutocomplete.getPlace();
-    let request = {
+
+    var searchRequest = {
+      location: startPlace.geometry.location,
+      radius: '5000',
+      query: '',
+      type: 'gas_station',
+    };
+
+    placeService.nearbySearch(searchRequest, nearbySearchCallback);
+
+    let directionsRequest = {
       origin: startPlace.geometry.location,
       destination: endPlace.geometry.location,
       travelMode: 'DRIVING',
     };
-    directionsService.route(request, function (result, status) {
+    directionsService.route(directionsRequest, function (result, status) {
       if (status == 'OK') {
         directionsRenderer.setDirections(result);
       } else {
@@ -59,9 +70,6 @@ function initMap() {
         );
       }
     });
-
-    createMarker(startPlace);
-    createMarker(endPlace);
   });
 }
 
@@ -85,28 +93,57 @@ function codeAddress() {
 function codeAddressCallback(results, status) {
   if (status == 'OK') {
     map.setCenter(results[0].geometry.location);
-
-    // request
-    var request = {
-      location: results[0].geometry.location,
-      radius: '5000',
-      query: '',
-      type: 'gas_station',
-    };
-
-    //service = new google.maps.places.PlacesService(map);
-    //service.nearbySearch(request, textSearchCallback);
   } else {
     alert('Geocode was not successful for the following reason: ' + status);
   }
 }
 
-function textSearchCallback(results, status) {
-  console.log(results);
+function nearbySearchCallback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       var place = results[i];
       createMarker(place);
     }
+
+    /*
+      GETTING DISTANCES USING DIRECTIONS MATRIX
+    */
+
+    transformedResults = results.map((res) => res.geometry.location);
+
+    let distanceMatrixReq1 = {
+      // distance from start to each gas station
+      origins: [startPlace.geometry.location],
+      destinations: transformedResults,
+      travelMode: 'DRIVING',
+    };
+
+    let distanceMatrixReq2 = {
+      // distance from each gas station to end
+      origins: transformedResults,
+      destinations: [endPlace.geometry.location],
+      travelMode: 'DRIVING',
+    };
+
+    distanceMatrixService.getDistanceMatrix(
+      distanceMatrixReq1,
+      distanceMatrixCallback1
+    );
+
+    distanceMatrixService.getDistanceMatrix(
+      distanceMatrixReq2,
+      distanceMatrixCallback2
+    );
+    function distanceMatrixCallback1(results, status) {
+      console.log(results);
+    }
+
+    function distanceMatrixCallback2(results, status) {
+      console.log(results);
+    }
+  } else {
+    alert(
+      'nearby search was not successful for the following reason: ' + status
+    );
   }
 }
